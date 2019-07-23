ce0317027ea3a937a36b8e04225c6a9cf7a3623c
krikaz <pascal.bou@protonmail.com>
2019-07-23 14:41:32 +0200

    added session and knex-session

--- index.js
+++ index.js
@@ -1,58 +1,36 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const md5 = require('md5');
+
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const Users = require('./users/users-model.js');
 const server = express();
 
 server.use(express.json());
+server.use(session({
+  name: 'sessionId', // name of the cookie
+  secret: 'keep it secret, keep it long', // we intend to encrypt
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: true,
+  // extra chunk of config
+  store: new KnexSessionStore({
+    knex: require('./database/dbConfig.js'), // configured instance of knex
+    tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+    sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+    createtable: true, // if the table does not exist, it will create it automatically
+    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+  }),
+}));
 
 server.get('/', (req, res) => {
 	res.send("It's working!");
 });
-
-// 1- build a custom middleware
-// that can check username & password
-// so that logic goes away from the login endpoint
-
-// 2- build a custom bcrypt
-//    - uses md5
-//    - uses a unique random salt
-//    - runs the hashing a configurable number of times
-//    - has a helper to check whether a password matches a hash
-
-const sillyBcrypt = {
-	hash(rawPassword, iterations) {
-		// computes random salt
-		// md5 the cancatenated rawPassword + salt
-		// does it "iterations" number of times
-		let randomSalt = new Date().getTime();
-		let result = rawPassword + randomSalt;
-		for (let i = 0; i < iterations; i++) {
-			result = md5(result);
-		}
-		return result;
-	},
-
-	compare(rawPassword, sillyBcryptHash) {
-		// pull the number of iterations and the salt from the sillyBcryptHash
-		// recompute a hash
-		// check that results are identical
-		// let user = Users.findBy({ username }).first();
-		// let result = rawPassword + sillyBcryptHash.userSalt;
-		// for (let i = 0; i < iterations; i++) {
-		// 	result = md5(result);
-		// }
-		// return result === user.password;
-	},
-};
-
-function restricted(req, res, next) {
-	// checks req.header for username & password
-	// performs the authentication
-	// if auths -> next()
-	// res.json('no way')
-}
 
 function checkCredentialsInBody(req, res, next) {
 	// checks req.body for username and password
@@ -73,16 +51,9 @@ function checkCredentialsInBody(req, res, next) {
 		});
 }
 
-server.post('/api/register', (req, res) => {
-	// we use bcrypt here to hash password and saved the hashed thing
-	// alg$12$my_saltxxxxxxxxxxxxxxxxxxx"
-	// we save that into the db instead of the plain text password
+server.post('/api/auth/register', (req, res) => {
 	let user = req.body;
 	user.password = bcrypt.hashSync(user.password, 12);
-	// use a very slow hashing function
-	// generate a random long salt
-	// hash the password together with the salt
-	// the result will be hashed again etc etc etc 84932579283742398
 
 	Users.add(user)
 		.then(saved => {
@@ -93,9 +64,7 @@ server.post('/api/register', (req, res) => {
 		});
 });
 
-server.post('/api/login', checkCredentialsInBody, (req, res) => {
-	// use bcrypt to compara the saved hash
-	// against the result of hashing again the provided password
+server.post('/api/auth/login', checkCredentialsInBody, (req, res) => {
 	let { username } = req.body;
 
 	Users.findBy({ username })
@@ -105,7 +74,7 @@ server.post('/api/login', checkCredentialsInBody, (req, res) => {
 		});
 });
 
-server.get('/api/users', restricted, (req, res) => {
+server.get('/api/users', (req, res) => {
 	Users.find()
 		.then(users => {
 			res.json(users);
@@ -113,5 +82,5 @@ server.get('/api/users', restricted, (req, res) => {
 		.catch(err => res.send(err));
 });
 
-const port = 4000;
+const port = 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
